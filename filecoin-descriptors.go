b0050930e2007a7cc367d4409c0ad936f7af54cf
Thijs van Hoof <thijs.v.h@gmail.com>
2023-10-20 16:45:33 +0200

    Remove hyperspace config

--- filecoin-descriptors.go
+++ filecoin-descriptors.go
@@ -17,7 +17,6 @@ import (
 var apiUrls = []string{
 	"https://api.node.glif.io/rpc/v1",
 	"https://api.calibration.node.glif.io/rpc/v1",
-	"https://api.hyperspace.node.glif.io/rpc/v1",
 }
 
 func main() {
